7e7268aeb618f4d46193b89be59bd36d1e90c187
oemel09 <oemel09@googlemail.com>
2018-09-20 22:35:04 -0500

    replaced tabs with spaces

--- src/de/oemel09/acme/Acme.java
+++ src/de/oemel09/acme/Acme.java
@@ -8,112 +8,112 @@ import java.util.HashMap;
 
 public class Acme {
 
-	private static final String[] dayAbbreviation = { "MO", "TU", "WE", "TH", "FR", "SA", "SU" };
-	
-	private static final int[] weekdayRange = { 0, 900, 1800, 2400 };
-	private static final int[] weekendRange = { 0, 900, 1800, 2400 };
-	private static final int[][] rangeMatrix = {
-			weekdayRange, 
-			weekdayRange,
-			weekdayRange,
-			weekdayRange,
-			weekdayRange,
-			weekendRange,
-			weekendRange,
-	};
-	
-	private static final int[] weekdayRate = { 25, 15, 20 };
-	private static final int[] weekendRate = { 30, 20, 25 };
-	private static final int[][] rateMatrix = { 
-			weekdayRate, 
-			weekdayRate, 
-			weekdayRate, 
-			weekdayRate, 
-			weekdayRate, 
-			weekendRate,
-			weekendRate,		
-	};
+    private static final String[] dayAbbreviation = { "MO", "TU", "WE", "TH", "FR", "SA", "SU" };
+    
+    private static final int[] weekdayRange = { 0, 900, 1800, 2400 };
+    private static final int[] weekendRange = { 0, 900, 1800, 2400 };
+    private static final int[][] rangeMatrix = {
+            weekdayRange, 
+            weekdayRange,
+            weekdayRange,
+            weekdayRange,
+            weekdayRange,
+            weekendRange,
+            weekendRange,
+    };
+    
+    private static final int[] weekdayRate = { 25, 15, 20 };
+    private static final int[] weekendRate = { 30, 20, 25 };
+    private static final int[][] rateMatrix = { 
+            weekdayRate, 
+            weekdayRate, 
+            weekdayRate, 
+            weekdayRate, 
+            weekdayRate, 
+            weekendRate,
+            weekendRate,        
+    };
 
-	private ArrayList<TimeEntry> timeEntries;
-	private HashMap<String, int[]> ranges;
-	private HashMap<String, int[]> rates;
+    private ArrayList<TimeEntry> timeEntries;
+    private HashMap<String, int[]> ranges;
+    private HashMap<String, int[]> rates;
 
-	public static void main(String[] args) throws IOException {
-		Acme acme = new Acme("input.txt");
-		for (TimeEntry te : acme.getTimeEntries()) {
-			acme.calculateSalary(te);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        Acme acme = new Acme("input.txt");
+        for (TimeEntry te : acme.getTimeEntries()) {
+            acme.calculateSalary(te);
+        }
+    }
 
-	public Acme(String fileName) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(fileName));
-		timeEntries = new ArrayList<>();
-		String row;
-		while ((row = br.readLine()) != null) {
-			timeEntries.add(new TimeEntry(row));
-		}
-		br.close();
-		
-		createRanges();
-		createRates();
-	}
+    public Acme(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        timeEntries = new ArrayList<>();
+        String row;
+        while ((row = br.readLine()) != null) {
+            timeEntries.add(new TimeEntry(row));
+        }
+        br.close();
+        
+        createRanges();
+        createRates();
+    }
 
-	private void createRanges() {
-		ranges = new HashMap<>();
-		for(int i = 0; i < 7; i++) {
-			ranges.put(dayAbbreviation[i], rangeMatrix[i]);
-		}
-	}
-	
-	private void createRates() {
-		rates = new HashMap<>();
-		for(int i = 0; i < 7; i++) {
-			rates.put(dayAbbreviation[i], rateMatrix[i]);
-		}
-	}
-	
-	private void calculateSalary(TimeEntry timeEntry) {
-		int salary = 0;
-		for (WorkDay wd : timeEntry.getDays()) {
-			salary += getSalary(wd);
-		}
-		String output = String.format("The amount to pay %s is: %d USD", timeEntry.getName(), salary);
-		System.out.println(output);
-	}
+    private void createRanges() {
+        ranges = new HashMap<>();
+        for(int i = 0; i < 7; i++) {
+            ranges.put(dayAbbreviation[i], rangeMatrix[i]);
+        }
+    }
+    
+    private void createRates() {
+        rates = new HashMap<>();
+        for(int i = 0; i < 7; i++) {
+            rates.put(dayAbbreviation[i], rateMatrix[i]);
+        }
+    }
+    
+    private void calculateSalary(TimeEntry timeEntry) {
+        int salary = 0;
+        for (WorkDay wd : timeEntry.getDays()) {
+            salary += getSalary(wd);
+        }
+        String output = String.format("The amount to pay %s is: %d USD", timeEntry.getName(), salary);
+        System.out.println(output);
+    }
 
-	private int getSalary(WorkDay workDay) {
-		int amount = 0;
-		String day = workDay.getDay();
-		int start = workDay.getStart();
-		int end = workDay.getEnd();
-		
-		// first range
-		amount += Math.max(0, ranges.get(day)[1] - start) / 100 * rates.get(day)[0];
-		amount -= Math.max(0, ranges.get(day)[1] - end) / 100 * rates.get(day)[0];
+    private int getSalary(WorkDay workDay) {
+        int amount = 0;
+        String day = workDay.getDay();
+        int start = workDay.getStart();
+        int end = workDay.getEnd();
+        
+        // first range
+        amount += Math.max(0, ranges.get(day)[1] - start) / 100 * rates.get(day)[0];
+        amount -= Math.max(0, ranges.get(day)[1] - end) / 100 * rates.get(day)[0];
 
-		// first we assume the employee worked the whole second range
-		int interval = ranges.get(day)[2] - ranges.get(day)[1];
-		amount += (interval) / 100 * rates.get(day)[1];
-		
-		// then we subtract the hours we assumed too much
-		if(end < ranges.get(day)[2]) {
-			// when employee worked only in first or in the first and second range 
-			amount -= Math.max(0, start - ranges.get(day)[1]) / 100 * rates.get(day)[1];
-			amount -= Math.min(interval, ranges.get(day)[2] - end) / 100 * rates.get(day)[1];	
-		} else if(start > ranges.get(day)[1]) {
-			// when employee worked in the second and third or only in the third range
-			amount -= Math.min(interval, start - ranges.get(day)[1]) / 100 * rates.get(day)[1];
-			amount -= Math.max(0, ranges.get(day)[2] - end) / 100 * rates.get(day)[1];	
-		}
+        // first we assume the employee worked the whole second range
+        int interval = ranges.get(day)[2] - ranges.get(day)[1];
+        amount += (interval) / 100 * rates.get(day)[1];
+        
+        // then we subtract the hours we assumed too much
+        if(end < ranges.get(day)[2]) {
+            // when employee worked only in first or in the first and second range 
+            amount -= Math.max(0, start - ranges.get(day)[1]) / 100 * rates.get(day)[1];
+            amount -= Math.min(interval, ranges.get(day)[2] - end) / 100 * rates.get(day)[1];    
+        } else if(start > ranges.get(day)[1]) {
+            // when employee worked in the second and third or only in the third range
+            amount -= Math.min(interval, start - ranges.get(day)[1]) / 100 * rates.get(day)[1];
+            amount -= Math.max(0, ranges.get(day)[2] - end) / 100 * rates.get(day)[1];    
+        }
 
-		// third range
-		amount += Math.max(0, end - ranges.get(day)[2]) / 100 * rates.get(day)[2];
-		amount -= Math.max(0, start - ranges.get(day)[2]) / 100 * rates.get(day)[2];
+        // third range
+        amount += Math.max(0, end - ranges.get(day)[2]) / 100 * rates.get(day)[2];
+        amount -= Math.max(0, start - ranges.get(day)[2]) / 100 * rates.get(day)[2];
 
-		return amount;
-	}
+        return amount;
+    }
 
-	public ArrayList<TimeEntry> getTimeEntries() {
-		return timeEntries;
-	}
+    public ArrayList<TimeEntry> getTimeEntries() {
+        return timeEntries;
+    }
 }
--- src/de/oemel09/acme/WorkDay.java
+++ src/de/oemel09/acme/WorkDay.java
@@ -1,25 +1,25 @@
 package de.oemel09.acme;
 
 public class WorkDay {
-	
-	private String day;
-	private int start, end;
+    
+    private String day;
+    private int start, end;
 
-	public WorkDay(String day, int start, int end) {
-		this.day = day;
-		this.start = start;
-		this.end = end;
-	}
+    public WorkDay(String day, int start, int end) {
+        this.day = day;
+        this.start = start;
+        this.end = end;
+    }
 
-	public String getDay() {
-		return day;
-	}
+    public String getDay() {
+        return day;
+    }
 
-	public int getStart() {
-		return start;
-	}
+    public int getStart() {
+        return start;
+    }
 
-	public int getEnd() {
-		return end;
-	}
+    public int getEnd() {
+        return end;
+    }
 }
