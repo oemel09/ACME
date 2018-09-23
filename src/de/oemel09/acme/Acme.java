98fe661ea93bd2ddb230ecaa2fefa2499a23b959
oemel09 <oemel09@googlemail.com>
2018-09-22 22:23:10 -0500

    prepared for testing

--- src/de/oemel09/acme/Acme.java
+++ src/de/oemel09/acme/Acme.java
@@ -9,11 +9,11 @@ import java.util.HashMap;
 public class Acme {
 
     private static final String[] dayAbbreviation = { "MO", "TU", "WE", "TH", "FR", "SA", "SU" };
-    
+
     private static final int[] weekdayRange = { 0, 900, 1800, 2400 };
     private static final int[] weekendRange = { 0, 900, 1800, 2400 };
     private static final int[][] rangeMatrix = {
-            weekdayRange, 
+            weekdayRange,
             weekdayRange,
             weekdayRange,
             weekdayRange,
@@ -21,17 +21,17 @@ public class Acme {
             weekendRange,
             weekendRange,
     };
-    
+
     private static final int[] weekdayRate = { 25, 15, 20 };
     private static final int[] weekendRate = { 30, 20, 25 };
     private static final int[][] rateMatrix = { 
-            weekdayRate, 
-            weekdayRate, 
-            weekdayRate, 
-            weekdayRate, 
-            weekdayRate, 
+            weekdayRate,
+            weekdayRate,
+            weekdayRate,
+            weekdayRate,
+            weekdayRate,
             weekendRate,
-            weekendRate,        
+            weekendRate,
     };
 
     private ArrayList<TimeEntry> timeEntries;
@@ -53,25 +53,25 @@ public class Acme {
             timeEntries.add(new TimeEntry(row));
         }
         br.close();
-        
+
         createRanges();
         createRates();
     }
 
     private void createRanges() {
         ranges = new HashMap<>();
-        for(int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) {
             ranges.put(dayAbbreviation[i], rangeMatrix[i]);
         }
     }
-    
+
     private void createRates() {
         rates = new HashMap<>();
-        for(int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) {
             rates.put(dayAbbreviation[i], rateMatrix[i]);
         }
     }
-    
+
     private void calculateSalary(TimeEntry timeEntry) {
         int salary = 0;
         for (WorkDay wd : timeEntry.getDays()) {
@@ -81,12 +81,13 @@ public class Acme {
         System.out.println(output);
     }
 
-    private int getDailySalary(WorkDay workDay) {
+    // method will get tested
+    public int getDailySalary(WorkDay workDay) {
         int amount = 0;
         String day = workDay.getDay();
         int start = workDay.getStart();
         int end = workDay.getEnd();
-        
+
         // first range
         amount += Math.max(0, ranges.get(day)[1] - start) / 100 * rates.get(day)[0];
         amount -= Math.max(0, ranges.get(day)[1] - end) / 100 * rates.get(day)[0];
@@ -94,16 +95,16 @@ public class Acme {
         // first we assume the employee worked the whole second range
         int interval = ranges.get(day)[2] - ranges.get(day)[1];
         amount += (interval) / 100 * rates.get(day)[1];
-        
+
         // then we subtract the hours we assumed too much
-        if(end < ranges.get(day)[2]) {
-            // when employee worked only in first or in the first and second range 
+        if (end < ranges.get(day)[2]) {
+            // when employee worked only in first or in the first and second range
             amount -= Math.max(0, start - ranges.get(day)[1]) / 100 * rates.get(day)[1];
-            amount -= Math.min(interval, ranges.get(day)[2] - end) / 100 * rates.get(day)[1];    
-        } else if(start > ranges.get(day)[1]) {
+            amount -= Math.min(interval, ranges.get(day)[2] - end) / 100 * rates.get(day)[1];
+        } else if (start > ranges.get(day)[1]) {
             // when employee worked in the second and third or only in the third range
             amount -= Math.min(interval, start - ranges.get(day)[1]) / 100 * rates.get(day)[1];
-            amount -= Math.max(0, ranges.get(day)[2] - end) / 100 * rates.get(day)[1];    
+            amount -= Math.max(0, ranges.get(day)[2] - end) / 100 * rates.get(day)[1];
         }
 
         // third range
@@ -113,6 +114,7 @@ public class Acme {
         return amount;
     }
 
+    // method will get tested
     public ArrayList<TimeEntry> getTimeEntries() {
         return timeEntries;
     }
