6a7f2037a8321cba1bd44c903ef3dd73ee610e7f
oemel09 <oemel09@googlemail.com>
2018-09-22 22:24:06 -0500

    added more input validation

--- src/de/oemel09/acme/TimeEntry.java
+++ src/de/oemel09/acme/TimeEntry.java
@@ -7,9 +7,10 @@ import java.util.regex.Pattern;
 public class TimeEntry {
 
     private String name;
-    private ArrayList<WorkDay> days = new ArrayList<>();
+    private ArrayList<WorkDay> days;
 
     public TimeEntry(String entry) throws IllegalArgumentException {
+        days = new ArrayList<>();
         parseEntry(entry);
     }
 
@@ -25,9 +26,14 @@ public class TimeEntry {
             Pattern workDayPattern = Pattern.compile(workDayRegex);
             Matcher matcher = workDayPattern.matcher(entry);
             while (matcher.find()) {
-                this.days.add(new WorkDay(matcher.group(1), 
-                        Integer.parseInt(matcher.group(2) + matcher.group(3)),
-                        Integer.parseInt(matcher.group(4) + matcher.group(5))));
+                int start = Integer.parseInt(matcher.group(2) + matcher.group(3));
+                int end = Integer.parseInt(matcher.group(4) + matcher.group(5));
+                
+                if (end < start) {
+                    throw new IllegalArgumentException("end before start");
+                }
+
+                this.days.add(new WorkDay(matcher.group(1), start, end));
             }
         } else {
             throw new IllegalArgumentException();
